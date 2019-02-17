6e55c2dbcecde6bcf379892941819d6a4db9575f
DalinSeivewright <dalin.seivewright@gmail.com>
2019-02-16 19:34:39 -0600

    Updated Deploy Script
    Copies JAR to a specific spot now which will be setup by the install script.
    Also restarts the service automatically.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # TODO, use a key-file rather than requiring manual authentication
-# TODO, do a file rename to something consistent
-# TODO, add to boot config on pi if not already done
-./gradlew build && rsync -avzhe ssh ./build/libs/pi-naut-1.0.0-SNAPSHOT-all.jar pi@raspberrypi.local:
+./gradlew build \\
+  && rsync -avzhe ssh ./build/libs/pi-naut-1.0.0-SNAPSHOT-all.jar pi@raspberrypi.local/opt/github-notify/github-notify.jar \\
+  && ssh pi@raspberrypi.local "sudo service restart github-notify"
