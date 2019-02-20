4105e1f00bf61dd89e99463186d644f656dbcb50
Dalin Seivewright <dalin.seivewright@gmail.com>
2019-02-20 11:57:07 -0600

    Fixing deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 # TODO, use a key-file rather than requiring manual authentication
-./gradlew build \\
-  && rsync -avzhe ssh ./build/libs/pi-naut-1.0.0-SNAPSHOT-all.jar pi@raspberrypi.local/opt/github-notify/github-notify.jar \\
+./gradlew build \
+  && rsync -avzhe ssh ./build/libs/pi-naut-1.0.0-SNAPSHOT-all.jar pi@raspberrypi.local/opt/pi-naut/github-notify.jar \
   && ssh pi@raspberrypi.local "sudo service restart github-notify"
