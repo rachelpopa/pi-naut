79b3aa6f6bb6fba8965216299b067f04303eb637
Jeramy Toplak <jermay.toplak@fcc-fac.ca>
2019-04-23 09:59:44 -0600

    need to add dummy job for seed

--- src/main/java/pi/naut/ApplicationState.java
+++ src/main/java/pi/naut/ApplicationState.java
@@ -1,8 +1,21 @@
 package pi.naut;
 
+import io.micronaut.context.event.ApplicationEventPublisher;
+import io.micronaut.scheduling.annotation.Scheduled;
+import pi.naut.gpio.bonnet.display.RefreshDisplayEvent;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class ApplicationState {
+
+	@Inject
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	@Scheduled(fixedRate = "1s")
+	void dummyJob() {
+		applicationEventPublisher.publishEvent(new RefreshDisplayEvent());
+	}
 
 }
