1494bb3a843c11e6e16cd7bf6f68008fdbb0edf1
Dalin <dalin.seivewright@gmail.com>
2019-04-25 15:18:59 -0600

    Committing new debug script and fixed deploy script.  Need to still update for hostname configuration.

--- debug.sh
+++ debug.sh
@@ -2,4 +2,4 @@
 ./gradlew build \
   && ssh pi@raspberrypi.local "sudo systemctl stop pi-naut" \
   && rsync -avzhe ssh ./build/libs/pi-naut-*-all.jar pi@raspberrypi.local:/opt/pi-naut/pi-naut.jar \
-  && ssh pi@raspberrypi.local "sudo systemctl restart pi-naut"
+  && ssh pi@raspberrypi.local "/usr/bin/java -jar /opt/pi-naut/pi-naut.jar"
