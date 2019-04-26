daf0521c6e38c2572d09e6a8bad103a7f56e85f0
DalinSeivewright <dalin.seivewright@gmail.com>
2019-04-25 20:46:46 -0600

    possible fix for ctrl+c

--- debug.sh
+++ debug.sh
@@ -3,4 +3,4 @@ RPI_HOSTNAME=$(cat RPI_HOSTNAME)
 ./gradlew build \
   && ssh pi@${RPI_HOSTNAME}.local "sudo systemctl stop pi-naut" \
   && rsync -avzhe ssh ./build/libs/pi-naut-*-all.jar pi@${RPI_HOSTNAME}.local:/home/pi/pi-naut.jar \
-  && ssh pi@${RPI_HOSTNAME}.local "/usr/bin/java -jar /home/pi/pi-naut.jar -verbose"
+  && ssh -t -t pi@${RPI_HOSTNAME}.local "/usr/bin/java -jar /home/pi/pi-naut.jar -verbose"
