e9b2aca7a0fb8bbb1c66d91889aab8344b642cf6
Jeramy Toplak <jermay.toplak@fcc-fac.ca>
2019-04-25 21:05:42 -0600

    fix event hook on shutdown

--- src/main/java/pi/naut/gpio/controller/PinController.java
+++ src/main/java/pi/naut/gpio/controller/PinController.java
@@ -4,34 +4,22 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinPullResistance;
-import io.micronaut.discovery.event.ServiceShutdownEvent;
-import io.micronaut.runtime.event.annotation.EventListener;
 import pi.naut.gpio.PinConfiguration;
 
 import javax.inject.Singleton;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Singleton
 public class PinController {
 
 	private final GpioController gpioController = GpioFactory.getInstance();
-	private Map<String, GpioPinDigitalInput> inputPins = new ConcurrentHashMap<>();
+	private Map<String, GpioPinDigitalInput> inputPins = new HashMap<>();
 
 	PinController() {
-		System.out.println("--> GPIO STARTUP");
 		PinConfiguration.DIGITAL_INPUT_PINS.forEach((key, value) ->
-				inputPins.computeIfAbsent(key, e -> {
-					GpioPinDigitalInput gpioPinDigitalInput = gpioController.provisionDigitalInputPin(value, key, PinPullResistance.PULL_UP);
-					gpioPinDigitalInput.setShutdownOptions(false);
-					return gpioPinDigitalInput;
-				}));
-	}
-
-	@EventListener
-	void onShutdown(ServiceShutdownEvent event) {
-		System.out.println("--> GPIO SHUTDOWN");
-		gpioController.shutdown();
+				inputPins.computeIfAbsent(key, e ->
+						gpioController.provisionDigitalInputPin(value, key, PinPullResistance.PULL_UP)));
 	}
 
 	public Map<String, GpioPinDigitalInput> getInputPins() {
