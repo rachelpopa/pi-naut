e2e11ae78b5cad91c70a24e60bebd1c526120bd7
Dalin <dalin.seivewright@gmail.com>
2019-04-25 18:46:31 -0600

    We now debug from the home directory instead of the service directory.  Added docs for scripts.

--- debug.sh
+++ debug.sh
@@ -2,5 +2,5 @@
 RPI_HOSTNAME=$(cat RPI_HOSTNAME)
 ./gradlew build \
   && ssh pi@${RPI_HOSTNAME}.local "sudo systemctl stop pi-naut" \
-  && rsync -avzhe ssh ./build/libs/pi-naut-*-all.jar pi@${RPI_HOSTNAME}.local:/opt/pi-naut/pi-naut.jar \
-  && ssh pi@${RPI_HOSTNAME}.local "/usr/bin/java -jar /opt/pi-naut/pi-naut.jar"
+  && rsync -avzhe ssh ./build/libs/pi-naut-*-all.jar pi@${RPI_HOSTNAME}.local:/home/pi/pi-naut.jar \
+  && ssh pi@${RPI_HOSTNAME}.local "/usr/bin/java -jar /home/pi/pi-naut.jar -verbose"
